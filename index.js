8cb2c965beabb0ac70c4f7a02602a51eb2de9139
ViniciusNogueiraS <v.nog@outlook.com>
2020-08-02 00:12:57 -0300

    Global Chat

--- index.js
+++ index.js
@@ -1,20 +1,67 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+const { connected } = require('process');
+var port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-// app.use(express.static(__dirname + '/public/'));
+var users = [];
+var messages = [];
+
+app.post('/chat', function(req, res){
+  res.sendFile(__dirname + '/chat.html');
+});
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  console.log(io.engine.clientsCount+" sockets ativos!");//quantidade de clientes online
+  console.log(socket.id);
+  
+  socket.on('signIn', function(conn){
+    
+    if (users.length == 0){
+      users.push(conn.user);
+    }else{
+      var user = users.find(u => conn.user && u.id === conn.user.id);
+      if (!user) {
+        users.push(conn.user);
+      }
+    }
+    
+    io.emit('usersOn', users);
+    socket.emit('loadChat', [messages, socket.id]);
+    console.log(conn.user.nickname+' entrou no chat!');
+  });
+
+  socket.on('chat', function(msg){//evento de envio de msg;
+    if (messages.length >= 20){
+      messages = [];
+    }
+
+    messages.push(msg);
+
+    io.emit('chat', msg);
+  });
+
+  socket.on('logout', function(user){
+    var index = users.findIndex(u => u.id === user.id);
+    if (index > -1) {
+      users.splice(index, 1);//remove da lista
+      console.log(user.nickname+' saiu!');
+      io.emit('usersOn', users);
+    }
   });
 });
 
-http.listen(8000, function(){
-  console.log('listening on *:8000');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
